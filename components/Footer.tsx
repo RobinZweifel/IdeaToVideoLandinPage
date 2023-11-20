939a43362945b4ac24a7185ed3feef349b7c72d2
Robin Zweifel <robinzweifel2003@gmail.com>
2023-11-20 19:06:12 +0100

    init

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { V0Logo } from './v0logo';
 const Footer = () => (
   <footer>
     <div className="custom-screen pt-16">
-      <div className="mt-10 py-10 border-t items-center justify-between flex">
+      {/* <div className="mt-10 py-10 border-t items-center justify-between flex">
         <p className="text-gray-600">
           Created by{' '}
           <a
@@ -31,7 +31,7 @@ const Footer = () => (
             <V0Logo width={25} />
           </a>
         </div>
-      </div>
+      </div> */}
     </div>
   </footer>
 );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,27 +11,26 @@ export default function Hero() {
       <div className="custom-screen pt-28 text-gray-600">
         <div className="space-y-5 max-w-4xl mx-auto text-center">
           <h1 className="text-4xl text-gray-800 font-extrabold mx-auto sm:text-6xl">
-            Generate your next AI QR Code in seconds
+            Generate your advertising video in seconds
           </h1>
           <p className="max-w-xl mx-auto">
-            QRGPT makes it simple for you to generate cool looking AI QR codes
-            in seconds, completely for free.
+            We use the power of GPT to generate a video based on a prompt.
           </p>
           <div className="flex items-center justify-center gap-x-3 font-medium text-sm">
             <NavLink
               href="/start"
               className="text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900 "
             >
-              Generate your QR Code
+              Generate your video
             </NavLink>
-            <NavLink
+            {/* <NavLink
               target="_blank"
               href="https://github.com/Nutlope/qrGPT"
               className="text-gray-700 border hover:bg-gray-50"
               scroll={false}
             >
               Learn more
-            </NavLink>
+            </NavLink> */}
           </div>
           <div className="grid sm:grid-cols-3 grid-cols-2 gap-4 pt-10">
             {heroImages.map((image, idx) => (
