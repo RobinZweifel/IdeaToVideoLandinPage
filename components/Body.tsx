0e9535001c966726b2ab48c0d1da16f3112ec1b2
Robin Zweifel <robinzweifel2003@gmail.com>
2023-11-24 00:12:06 +0100

    form

--- components/Body.tsx
+++ components/Body.tsx
@@ -3,38 +3,66 @@
 import { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 
-const Body = ({
-  
-}: {
-  imageUrl?: string;
-  prompt?: string;
-  redirectUrl?: string;
-  modelLatency?: number;
-  id?: string;
-}) => {
-  const [submittedURL] = useState<string | null>(null);
+const Body = () => {
+  // const [submittedURL] = useState<string | null>(null);
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [text, setText] = useState('');
+  const [color1, setColor1] = useState('#000000'); // Default color black
+  const [color2, setColor2] = useState('#FFFFFF'); // Default color white
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    // Handle the form submission logic here
+  };
 
   return (
     <div className="flex justify-center items-center flex-col w-full lg:p-0 p-4 sm:mb-28 mb-0">
-      <div className="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 mt-10">
-        <div className="col-span-1">
-          <h1 className="text-3xl font-bold mb-10">Generate a video</h1>
-
-        </div>
-        <div className="col-span-1">
-          {submittedURL && (
-            <>
-              <h1 className="text-3xl font-bold sm:mb-5 mb-5 mt-5 sm:mt-0 sm:text-center text-left">
-                Your QR Code
-              </h1>
-              <div>
-                <div className="flex flex-col justify-center relative h-auto items-center">
-
-                </div>
-              </div>
-            </>
-          )}
-        </div>
+      <div className="max-w-6xl w-full mt-10">
+        <h1 className="text-3xl font-bold mb-10 text-center">Generate a Video</h1>
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4 items-start">
+          <input 
+            type="text"
+            placeholder="Enter title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="p-2 border rounded w-full"
+          />
+          <input 
+            type="text"
+            placeholder="Enter subtitle"
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
+            className="p-2 border rounded w-full"
+          />
+          <textarea 
+            placeholder="Enter text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="p-2 border rounded w-full"
+          />
+          <div className="flex items-center gap-2">
+            <input 
+              type="color"
+              value={color1}
+              onChange={(e) => setColor1(e.target.value)}
+              className="w-6 h-6 border rounded"
+            />
+            <span>{color1}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <input 
+              type="color"
+              value={color2}
+              onChange={(e) => setColor2(e.target.value)}
+              className="w-6 h-6 border rounded"
+            />
+            <span>{color2}</span>
+          </div>
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 float-left">
+            Submit
+          </button>
+        </form>
       </div>
       <Toaster />
     </div>
