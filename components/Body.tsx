4c61c9e208fe1f7283ae83c63008d6f5aa3c30f1
Robin Zweifel <robinzweifel2003@gmail.com>
2023-11-23 23:52:46 +0100

    fffa

--- components/Body.tsx
+++ components/Body.tsx
@@ -26,13 +26,6 @@ const Body = ({
 }) => {
   const [submittedURL] = useState<string | null>(null);
 
-  const router = useRouter();
-
-  const form = useForm<GenerateFormValues>({
-    resolver: zodResolver(generateFormSchema),
-    mode: 'onChange',
-  });
-
   return (
     <div className="flex justify-center items-center flex-col w-full lg:p-0 p-4 sm:mb-28 mb-0">
       <div className="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 mt-10">
